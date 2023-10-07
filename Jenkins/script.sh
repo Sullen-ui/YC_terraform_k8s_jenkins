f3a650acf53655f8bad57dce4feab37f2d0b17d4
Sullen-ui <maorlov.it@gmail.com>
2023-10-07 17:41:24 +0700

    add Jenkins pipeline and script for getting response status code

---
+++ Jenkins/script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+URL=$1
+RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
+
+if [ $RESPONSE_CODE -eq 200 ]; then
+    echo "success"
+    exit 0
+else
+    echo "failure. Response status code = $RESPONSE_CODE"
+    exit 1
+fi
